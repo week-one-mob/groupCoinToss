c564644f12a9def8cfa5cc263a445e294caad973
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-24 15:38:29 -0700

    set variables equal to objects in html doc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-
 // import our function
-import coinTossFunction from "coinTossFunction.js"
+import coinTossFunction from "./coinTossFunction.js"
 //reference our DOM elements
 const flipButton = document.getElementById("flip-button");
+const coinImage = document.getElementById("coin-img");
+const winsDisplay = document.getElementById("win");
+const lossesDisplay = document.getElementById("losses");
+const ourChoice = document.getElementById("heads-choice");
+
 // initialize some stuff
+let winCount = 0;
+let lossesCount = 0;
 
 // add event listener
 flipButton.addEventListener('click', () => {
-
+    const randomNumber = Math.random();
+    const flip = coinTossFunction(randomNumber);
+    console.log("banana");
 });
+
 //changee image display
 
 //show message
--- test/tests.js
+++ test/tests.js
@@ -2,25 +2,19 @@ import coinTossFunction from '../src/coinTossFunction.js';
 const test = QUnit.test;
 
 test('get heads for less then .5', function(assert) {
-    //Arrange
     const number = .49;
     const expected = "heads";
-    //Act 
-    const result = coinTossFunction(number);
-    // Call the function you're testing and set the result to a const
 
-    //Assert
+    const result = coinTossFunction(number);
+
     assert.equal(result, expected);
 });
 
 test('get tails for greater then or equal then 5', function(assert) {
-    //Arrange
     const number = .5;
     const expected = "tails";
-    //Act 
-    const result = coinTossFunction(number);
-    // Call the function you're testing and set the result to a const
 
-    //Assert
+    const result = coinTossFunction(number);
+  
     assert.equal(result, expected);
 });
